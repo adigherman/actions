408e1b4c4fee006f67b354fce6ae3fe47c4ed2a8
Adi G <adig@jhu.edu>
2020-07-13 16:28:12 -0400

    minor print adjustment

--- get-repo-name/index.js
+++ get-repo-name/index.js
@@ -1,9 +1,9 @@
 const core = require('@actions/core');
 
 try {
-  // `slug` input defined in action metadata file
+  // `github_slug` input defined in action metadata file
   const slug = core.getInput('github_slug');
-  console.log(`Slug is  ${slug}!`);
+  console.log(`repo slug is:  ${slug}`);
   const slug_array = slug.split("/")
   core.setOutput("repo_name", slug_array[1]);
 
