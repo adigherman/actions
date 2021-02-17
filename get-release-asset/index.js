baab62bcd1e39b2e919b6481b5dbcd589e728dc5
Adi G <adig@jhu.edu>
2021-02-17 09:53:55 -0500

    added get-release-asset action

---
+++ get-release-asset/index.js
@@ -0,0 +1,38 @@
+const core = require('@actions/core');
+
+try {
+  // `github_slug` input defined in action metadata file
+  const repo_name = core.getInput('repo_name');
+  const repo_version = core.getInput('repo_version');
+  const runner_os = core.getInput('runner_os');
+  const R_version = core.getInput('R_version');
+  
+  console.log(`Package:  ${repo_name}`);
+  console.log(`Version:  ${repo_version}`);
+  console.log(`OS:  ${runner_os}`);
+  
+  const asset_name = repo_name+'_'+repo_version;
+  const asset_path = 'check/'+repo_name+'_'+repo_version;
+  
+  switch(runner_os){
+    case 'macOS':
+      asset_name.concat('_R',R_version,'.tgz');
+      core.setOutput("asset_name", asset_name);
+      asset_path.concat('.tgz');
+      core.setOutput("asset_path", asset_path);
+      break;
+    case 'Windows':
+      asset_name.concat('_R',R_version,'.zip');
+      core.setOutput("asset_name", asset_name);
+      asset_path.concat('.zip');
+      core.setOutput("asset_path", asset_path);
+      break;
+    default:
+      asset_name.concat('_R_x86_64-pc-linux-gnu_R',R_version,'.tar.gz');
+      core.setOutput("asset_name", asset_name);
+      asset_path.concat('_R_x86_64-pc-linux-gnu.tar.gz');
+      core.setOutput("asset_path", asset_path);
+  }
+} catch (error) {
+  core.setFailed(error.message);
+}
