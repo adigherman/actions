9c615646c07f893b77d10456defeb50b8a4014ae
Adi G <adig@jhu.edu>
2021-03-23 12:45:59 -0400

    sysdeps action added

---
+++ install-sysdeps/linux.sh
@@ -0,0 +1,36 @@
+sudo apt-get install -y cmake ;    
+sudo apt-get install -y libssh-dev;
+sudo apt-get install -y jags
+sudo apt-get install -y libv8-dev;
+sudo apt-get install -y libfftw3-dev ;
+sudo apt-get install -y dcmtk ;
+sudo apt-get install -y libsecret-1-dev
+sudo apt-get install -y libpng-dev
+sudo apt-get install -y libpoppler-cpp-dev
+sudo apt-get install -y libhdf5-serial-dev libhdf5-dev ;
+sudo apt-get install -y mesa-common-dev libglu1-mesa-dev 
+sudo apt-get install -y libprotobuf-dev protobuf-compiler libprotoc-dev
+sudo apt-get install -y ffmpeg --allow-unauthenticated 
+sudo apt-get install -y libmagick++-dev ;
+sudo apt-get install -y ghostscript imagemagick
+sudo apt-get install -y libudunits2-dev ;
+sudo apt-get install -y libjq-dev ;
+sudo apt-get install -y libmng2 ; 
+sudo apt-get install -y libmng1 ; 
+sudo apt-get install -y libapparmor-dev libapparmor-devel
+sudo apt-get install -y imagej
+sudo apt-get install -y bowtie2
+sudo apt-get install -y install gdal
+sudo apt-get install -y insighttoolkit4-python libinsighttoolkit4-dev
+sudo apt-get install -y libav-tools
+sudo apt-get install -y libgit2-dev
+sudo apt-get install -y libtiff-dev
+sudo apt-get install -y unixodbc-dev
+sudo apt-get install -y libsodium-dev
+sudo apt-get install -y libtesseract-dev libleptonica-dev tesseract-ocr-eng
+sudo apt-get install -y wget
+sudo apt-get install -y libzmq3-dev
+sudo apt-get install -y udunits
+sudo apt-get install -y libwebp-dev
+sudo apt-get install -y cargo
+sudo apt-get install -y qgis
---
+++ install-sysdeps/macos.sh
@@ -0,0 +1,33 @@
+brew install -v cmake ;
+brew install -v libssh ;
+brew install -v jags; 
+brew install -v v8 ;
+brew install -v fftw ;
+brew install -v dcmtk ;
+brew install -v libsecret    
+brew install -v libpng
+brew install -v poppler    
+brew install -v hdf5
+brew install -v protobuf
+brew install -v gsl
+brew install -v ffmpeg    
+brew install -v imagemagick@6
+brew install -v imagemagick    
+brew install -v udunits ;
+brew install -v jq ;
+brew install -v imagej
+brew install homebrew/science/bowtie2
+brew install gdal
+brew install -v insighttoolkit
+brew install -v libav
+brew install libgit2
+brew install libtiff
+brew install unixodbc
+brew install libsodium
+brew install tesseract
+brew install wget
+brew install zeromq
+brew install udunits 
+brew install webp 
+brew install rust
+brew install qgis2
