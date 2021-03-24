ab027ed731078c64c72aa5ebad726595061228d6
Adi G <adig@jhu.edu>
2021-03-24 09:53:32 -0400

    test 2 [install-sysdeps-windows]

--- install-sysdeps-windows/pacman.sh
+++ install-sysdeps-windows/pacman.sh
@@ -1,4 +1,3 @@
 pacman -Syu
-pacman --noconfirm -S --needed mingw-w64-aom
-pacman --noconfirm -S --needed mingw-w64-argtable
-pacman -S mingw-w64-x86_64-argtable
+pacman --noconfirm -S mingw-w64-aom
+pacman --noconfirm -S mingw-w64-argtable
