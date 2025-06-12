ae1d52417a9e41d3ee9dabf839d918fc1ed08909
Adi G <adig@jhu.edu>
2025-06-12 08:25:08 -0400

    Update linux.sh
    try pip install

--- install-sysdeps-linux/linux.sh
+++ install-sysdeps-linux/linux.sh
@@ -31,4 +31,4 @@ sudo apt-get install -y cargo ;
 sudo apt-get install -y qgis ;
 sudo apt-get install -y libharfbuzz-dev libfribidi-dev ;
 sudo apt-get install -y tk-dev ;
-sudo apt-get install -y python-h5py ;
+sudo pip3 install h5py ;
